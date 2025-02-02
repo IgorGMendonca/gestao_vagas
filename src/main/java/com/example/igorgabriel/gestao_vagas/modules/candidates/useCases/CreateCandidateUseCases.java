9d0edc7a27ec5ae20deaf8a734373d70b30e54b6
Igor <igorgmariano010@outlook.com>
2025-02-02 20:42:41 -0300

    chore: encode candidate password

--- src/main/java/com/example/igorgabriel/gestao_vagas/modules/candidates/useCases/CreateCandidateUseCases.java
+++ src/main/java/com/example/igorgabriel/gestao_vagas/modules/candidates/useCases/CreateCandidateUseCases.java
@@ -1,6 +1,7 @@
 package com.example.igorgabriel.gestao_vagas.modules.candidates.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.igorgabriel.gestao_vagas.modules.candidates.CandidateEntity;
@@ -13,11 +14,17 @@ public class CreateCandidateUseCases {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUserName(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException("User already exists" + user.getUserName());
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
